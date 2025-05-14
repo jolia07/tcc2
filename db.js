b64a9b5b4f3d9f1a27b8510190498f8921b071ba
julia088 <juvisari144@gmail.com>
2025-05-14 14:29:22 -0300

    popup de instruções

--- db.js
+++ db.js
@@ -17,45 +17,6 @@ const pool = new Pool({
 async function criarTabelas() {
   try {
     await pool.query(`
-      create table if not exists curso(
-      id SERIAL PRIMARY KEY NOT NULL, 
-      nome varchar(255) not null unique
-      );
-    `);
-    console.log("Tabela 'curso' pronta!");
-
-    await pool.query(`
-      create table if not exists turma(
-      id SERIAL PRIMARY KEY NOT NULL,
-      nome VARCHAR(255) NOT NULL,
-      curso_id INT NOT NULL,
-      foreign key (curso_id) references curso(id) on delete cascade
-      );
-    `);
-    console.log("Tabela 'turma' pronta!");
-
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS laboratorio (
-          id SERIAL PRIMARY KEY NOT NULL,
-          cimatec int not null,
-          andar int not null,
-          sala varchar(50) not null
-      );
-    `);
-    console.log("Tabela 'laboratorio' pronta!");
-
-    await pool.query(`
-          CREATE TABLE IF NOT EXISTS materia (
-              id SERIAL PRIMARY KEY NOT NULL,
-              uc varchar(255) not null,
-              ch int not null,
-              curso_id INT NOT NULL,
-              foreign key (curso_id) references curso(id) on delete cascade
-          );
-    `);
-    console.log("Tabela 'disciplina' pronta!");
-
-    await pool.query(`
           CREATE TABLE IF NOT EXISTS usuarios (
               id SERIAL PRIMARY KEY NOT NULL,
               nome VARCHAR(255) NOT NULL,
@@ -82,26 +43,6 @@ async function criarTabelas() {
     console.log("Tabela 'reset_tokens' pronta!");
 
     await pool.query(`
-          CREATE TABLE IF NOT EXISTS aula (
-              id SERIAL PRIMARY KEY NOT NULL,
-              usuario_id int,
-              foreign key (usuario_id) references usuarios(id) on delete cascade,
-              curso_id int not null,
-              foreign key (curso_id) references curso(id) on delete cascade,
-              materia_id int,
-              foreign key (materia_id) references materia(id) on delete cascade,
-              turma_id int not null,
-              foreign key (turma_id) references turma(id) on delete cascade,
-              laboratorio_id int,
-              foreign key (laboratorio_id) references laboratorio(id) on delete cascade,
-              turno varchar(255) not null,
-              diasSemana varchar(255) NOT NULL,
-              dataInicio date not null
-          );
-    `);
-    console.log("Tabela 'aula' pronta!");
-
-    await pool.query(`
             CREATE TABLE IF NOT EXISTS importado (
               id SERIAL PRIMARY KEY,
               nome VARCHAR(255) NOT NULL,
@@ -126,7 +67,6 @@ async function criarTabelas() {
 }
 
 criarTabelas();
-
 
 pool.query('SELECT 1')
   .then(() => {
